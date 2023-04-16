520eac7371df593dec5409b9ce9e6f96d99f3fa9
Gordey Guselnikov <g.guselnikov@bi.zone>
2023-04-17 02:17:53 +0700

    Initial Commit

---
+++ MVC-N/Comments/Controller/CommentsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Guselnikov Gordey on 4/17/23.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+}
+
+extension CommentsViewController: UITableViewDelegate {}
+
+extension CommentsViewController: UITableViewDataSource {}
